5b3e9b4fdd8774e2ae78ebfbf2e4b858a4a2a739
morrej05 <40033044+morrej05@users.noreply.github.com>
2026-01-24 13:59:54 +0000

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
+import { AuthProvider } from './contexts/AuthContext';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <AuthProvider>
     <App />
-  </StrictMode>
+  </AuthProvider>
 );

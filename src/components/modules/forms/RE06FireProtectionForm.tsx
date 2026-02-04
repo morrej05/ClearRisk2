4c84bf477fc787e07932250fd41b5244959be51a
morrej05 <40033044+morrej05@users.noreply.github.com>
2026-02-04 21:33:03 +0000

    Updated RE06FireProtectionForm.tsx

--- src/components/modules/forms/RE06FireProtectionForm.tsx
+++ src/components/modules/forms/RE06FireProtectionForm.tsx
@@ -1,15 +1,35 @@
 import { useState } from 'react';
 import FloatingSaveBar from './FloatingSaveBar';
+import SectionGrade from '../../SectionGrade';
+import { updateSectionGrade } from '../../../utils/sectionGrades';
 
 export default function RE06FireProtectionForm() {
   const [saving, setSaving] = useState(false);
+  const [grade, setGrade] = useState(3);
 
   return (
     <>
       <div style={{ padding: 24 }}>
         <h2>RE06 is compiling</h2>
-        <button onClick={() => setSaving(!saving)}>Toggle Saving</button>
+
+        <div style={{ marginTop: 12 }}>
+          <SectionGrade
+            sectionKey="fire_protection"
+            sectionTitle="Fire Protection"
+            value={grade}
+            onChange={(v: number) => {
+              setGrade(v);
+              // this call is just to prove the import/export works
+              updateSectionGrade('test-doc-id', 'fire_protection', v);
+            }}
+          />
+        </div>
+
+        <div style={{ marginTop: 12 }}>
+          <button onClick={() => setSaving(!saving)}>Toggle Saving</button>
+        </div>
       </div>
+
       <FloatingSaveBar onSave={() => alert('save')} isSaving={saving} />
     </>
   );

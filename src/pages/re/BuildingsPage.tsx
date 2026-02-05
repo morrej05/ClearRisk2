cc7cc854f90ad6e8c151af428c4fde499c1db33b
morrej05 <40033044+morrej05@users.noreply.github.com>
2026-02-05 11:22:13 +0000

    Updated BuildingsPage.tsx

--- src/pages/re/BuildingsPage.tsx
+++ src/pages/re/BuildingsPage.tsx
@@ -1,8 +1,8 @@
 import { useParams } from 'react-router-dom';
-import BuildingsEditor from "../../components/re/BuildingsEditor";
+import BuildingsEditor from '../../components/re/BuildingsEditor';
 
 export default function BuildingsPage() {
-  const { documentId } = useParams<{ documentId: string }>();
-  if (!documentId) return <div className="p-4">Missing documentId</div>;
-  return <BuildingsEditor documentId={documentId} />;
+  const { id } = useParams<{ id: string }>();
+  if (!id) return <div className="p-4">Missing document id</div>;
+  return <BuildingsEditor documentId={id} />;
 }

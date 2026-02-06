d41f736e4248d4e3c7db4cce5b4ccd3f08b84309
morrej05 <40033044+morrej05@users.noreply.github.com>
2026-02-06 07:51:48 +0000

    Updated DocumentWorkspace.tsx

--- src/pages/documents/DocumentWorkspace.tsx
+++ src/pages/documents/DocumentWorkspace.tsx
@@ -15,8 +15,6 @@ import DocumentStatusBadge from '../../components/documents/DocumentStatusBadge'
 import OverallGradeWidget from '../../components/re/OverallGradeWidget';
 import ModuleSidebar from '../../components/modules/ModuleSidebar';
 
-console.log('[DocumentWorkspace] version=2026-02-06-A');
-
 // Modules with dedicated routes that should NOT be rendered in workspace
 const DEDICATED_MODULE_KEYS = new Set(['RE_06_FIRE_PROTECTION']);
 

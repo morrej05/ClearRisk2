f25a099a6152b071fa175fdc9f8b97c9b6cf9dc0
morrej05 <40033044+morrej05@users.noreply.github.com>
2026-02-08 10:40:58 +0000

    Updated AuthedLayout.tsx

--- src/components/AuthedLayout.tsx
+++ src/components/AuthedLayout.tsx
@@ -1,11 +1,7 @@
-import { ReactNode } from 'react';
-import { Outlet } from 'react-router-dom';
-import AppLayout from './AppLayout';
-
-interface AuthedLayoutProps {
-  children?: ReactNode;
-}
-
-export default function AuthedLayout({ children }: AuthedLayoutProps) {
-  return <AppLayout>{children ?? <Outlet />}</AppLayout>;
+export default function AuthedLayout() {
+  return (
+    <AppLayout>
+      <Outlet />
+    </AppLayout>
+  );
 }

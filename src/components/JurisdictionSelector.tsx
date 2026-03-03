0054dbf02f792e5e9c5f68b0f834c7d527e6cb96
morrej05 <40033044+morrej05@users.noreply.github.com>
2026-03-03 14:34:52 +0000

    Updated JurisdictionSelector.tsx

--- src/components/JurisdictionSelector.tsx
+++ src/components/JurisdictionSelector.tsx
@@ -29,10 +29,10 @@ export function JurisdictionSelector({
   const isDsearContext = product === 'DSEAR';
 
   console.log('[JURISDICTION DEBUG]', {
-  documentType,
-  isDsearContext,
-  currentJurisdiction,
-});
+    product,
+    isDsearContext,
+    currentJurisdiction,
+  });
 
   const normalizeForContext = (value: Jurisdiction | string) =>
     isDsearContext ? normalizeDsearJurisdiction(value) : normalizeJurisdiction(value);
